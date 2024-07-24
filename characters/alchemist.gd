7a05a0901536cd513dba1f35071ed11bb7e4b6a3
samleishman <samuel.leishman@sparkz.systems>
2024-07-24 13:59:03 -0600

    add animation player for alchemist

--- characters/alchemist.gd
+++ characters/alchemist.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@onready var ap: AnimationPlayer = $AnimationPlayer
+@onready var sprite: Sprite2D = $Sprite2D
 
 const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
@@ -10,16 +12,11 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	apply_gravity(delta)
+	handle_direction()
 	handle_jump()
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	update_animations()
+	
 	move_and_slide()
 
 
@@ -28,6 +25,22 @@ func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
+func handle_direction():
+	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	# flip sprite depending on direction
+	if direction != 0:
+		sprite.flip_h = (direction == -1)
+
 func handle_jump():
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+
+
+
+func update_animations():
+	if is_on_floor():
+		ap.play("idle")
